7cf8fbca2106b77f8e08be87d4a185b1f736c6ce
Brian Reece <bdreece@mtu.edu>
2024-03-18 22:49:14 -0400

    refactor(core): refactor session schema

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -11,64 +11,82 @@ import (
 
 const (
 	userKey     string = "user"
+	nonceKey    string = "nonce"
 	tokenKey    string = "token"
 	roomCodeKey string = "roomCode"
 
-    Name string = "__melodeon-session"
+	DefaultCookie string = "__melodeon-session"
+	NonceCookie   string = "__melodeon-nonce"
 )
 
 func init() {
-    gob.Register(new(sessionToken))
-    gob.Register(new(api.User))
+	gob.Register(new(sessionToken))
+	gob.Register(new(api.User))
 }
 
 type Session struct{ sessions.Session }
 
 func (session Session) RoomCode() *string {
-    if code, ok := session.Values[roomCodeKey].(string); ok {
-        return &code
-    }
+	if code, ok := session.Values[roomCodeKey].(string); ok && code != "" {
+		return &code
+	}
 
-    return nil
+	return nil
 }
 
+func (session Session) Nonce() *string {
+	if nonce, ok := session.Values[nonceKey].(string); ok && nonce != "" {
+		return &nonce
+	}
 
-func (session Session) User() *api.User  {
-    if user, ok := session.Values[userKey].(*api.User); ok {
-        return user
-    }
+	return nil
+}
 
-    return nil
+func (session Session) User() *api.User {
+	if user, ok := session.Values[userKey].(*api.User); ok && user != nil {
+		return user
+	}
+
+	return nil
 }
 
 func (session Session) Token() *api.Token {
-	t := session.Values[tokenKey].(*sessionToken)
-	exp, _ := time.Parse(time.RFC3339, t.ExpiresIn)
-	return &api.Token{
-		AccessToken:  t.AccessToken,
-		RefreshToken: t.RefreshToken,
-		TokenType:    t.TokenType,
-		Scope:        t.Scope,
-		ExpiresIn:    exp,
+	if t, ok := session.Values[tokenKey].(*sessionToken); ok && t != nil {
+		exp, _ := time.Parse(time.RFC3339, t.ExpiresIn)
+		return &api.Token{
+			AccessToken:  t.AccessToken,
+			RefreshToken: t.RefreshToken,
+			TokenType:    t.TokenType,
+			Scope:        t.Scope,
+			ExpiresIn:    exp,
+		}
 	}
-}
 
-func (session *Session) SetUser(user *api.User)  { session.Values[userKey] = user }
+	return nil
+}
 
 func (session *Session) SetRoomCode(code string) { session.Values[roomCodeKey] = code }
 
+func (session *Session) SetNonce(nonce string) { session.Values[nonceKey] = nonce }
+
+func (session *Session) SetUser(user *api.User) { session.Values[userKey] = user }
+
 func (session *Session) SetToken(token *api.Token) {
-    session.Values[tokenKey] = &sessionToken{
-        AccessToken: token.AccessToken,
-        RefreshToken: token.RefreshToken,
-        TokenType: token.TokenType,
-        Scope: token.Scope,
-        ExpiresIn: token.ExpiresIn.Format(time.RFC3339),
-    }
+	if token == nil {
+		session.Values[tokenKey] = nil
+	} else {
+		session.Values[tokenKey] = &sessionToken{
+			AccessToken:  token.AccessToken,
+			RefreshToken: token.RefreshToken,
+			TokenType:    token.TokenType,
+			Scope:        token.Scope,
+			ExpiresIn:    token.ExpiresIn.Format(time.RFC3339),
+		}
+	}
 }
 
 func (session *Session) Save(c echo.Context) error {
-    return session.Session.Save(c.Request(), c.Response().Writer)
+	return session.Session.Save(c.Request(), c.Response().Writer)
 }
 
 type sessionToken struct {
@@ -78,4 +96,3 @@ type sessionToken struct {
 	TokenType    string
 	ExpiresIn    string
 }
-
